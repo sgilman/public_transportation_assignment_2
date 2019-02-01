ea90a3accd50dabd35d8d4a9d0479c4a0ebe9e46
sgilman <sco.gil.777@gmail.com>
2019-02-01 05:06:10 -0500

    comments in revenue_vehicle_hrs.sql

--- revenue_vehicle_hrs.sql
+++ revenue_vehicle_hrs.sql
@@ -1,13 +1,13 @@
 CREATE OR REPLACE VIEW trip_vehicle_hrs as 
     SELECT
         t.trip_id,
-        MIN(st.departure_time) as earliest,
-        MAX(st.arrival_time) as latest,
+        MIN(st.departure_time) as earliest,  -- earliest departure time on the trip
+        MAX(st.arrival_time) as latest,	     -- latest arrival time on the trip
         EXTRACT(EPOCH FROM (
             to_timestamp(MAX(st.arrival_time), 'hh24:mi:ss') -
             to_timestamp(MIN(st.departure_time), 'hh24:mi:ss')::timestamp
-        )) / 3600 as vehicle_hrs,
-        MIN(r.route_desc) as route_desc
+        )) / 3600 as vehicle_hrs,             -- difference between latest and earliest time to get time on trip
+        MIN(r.route_desc) as route_desc       
 
     FROM
         gtfs_stop_times as st,
@@ -15,16 +15,16 @@ CREATE OR REPLACE VIEW trip_vehicle_hrs as
         gtfs_routes as r,
         calendar_dates as cd
     WHERE
-        st.trip_id = t.trip_id AND
+        st.trip_id = t.trip_id AND			 -- join gtfs_stop_times, gtfs_trips, gtfs_routes, calendar dates
         t.route_id = r.route_id AND
         t.service_id = cd.service_id AND
-        cd.date = '20190219' AND
-        r.route_desc LIKE '%Bus'
-    GROUP BY t.trip_id;
+        cd.date = '20190126' AND			  -- modify date to see services on that day (between 1/15 and 3/15)
+        r.route_desc LIKE '%Bus'			  -- only select bus services
+    GROUP BY t.trip_id;						  --group by trip_id to get aggregate statistics for each trip
 
 SELECT
 	route_desc,
-    SUM(vehicle_hrs)
-FROM
+    SUM(vehicle_hrs)							-- get sum of all trip times for 
+FROM	
 	trip_vehicle_hrs
 GROUP BY route_desc
